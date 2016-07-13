5cde3629466bee3c288a82f8be12baf081d62b31
Jason Chen <shchenchang@gmail.com>
2016-07-13 16:16:15 +0800

    添加了 Boost Type Index 示例

---
+++ case12/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#include <boost/type_index.hpp>
+
+class Widget
+{
+public:
+    string val;
+};
+
+std::vector<Widget> createVec()
+{
+    std::vector<Widget> vw;
+    vw.emplace_back(Widget());
+    vw.emplace_back(Widget());
+    vw.emplace_back(Widget());
+    return vw;
+}
+
+
+template <typename T>
+void f(const T& param)
+{
+    using std::count;
+    using boost::typeindex::type_id_with_cvr;
+
+    cout << "T = " << type_id_with_cvr<T>().pretty_name() << endl;
+    cout << "param = " << type_id_with_cvr<decltype(param) >().pretty_name() << endl;
+}
+
+int main()
+{
+    const auto & vw = createVec();
+    if (!vw.empty())
+    {
+        f(&vw[0]);
+    }
+
+    return 0;
+}
