124b1ac6e4e70f13ccd75d3c0c9af016c136c748
Jason Chen <shchenchang@gmail.com>
2018-11-14 17:16:58 +0800

    Test variadic parameter function.

--- case13/main.cpp
+++ case13/main.cpp
@@ -1,9 +1,49 @@
 #include <iostream>
 using namespace std;
 
+template <typename ... Args> void func(void(*f)(Args ...), Args ... args)
+{
+    (*f)(args ...);
+}
+
+void f1()
+{
+    cout << "f1()" << endl;
+}
+
+void f3(int n)
+{
+    cout << "f3_1p(" << n << ")" << endl;
+}
+
+void f3(int a, int b)
+{
+    cout << "f3_2p(" << a << ", " << b << ")" << endl;
+}
+
+
+void f3(int a, int b, int c)
+{
+    cout << "f3_3p(" << a << ", " << b << ", " << c << ")" << endl;
+}
+
+template <typename ... Args> void f3(int a, int b, int c, Args ... args)
+{
+    f3(a, b, c);
+    f3(args ...);
+}
+
 int main()
 {
     cout << "hello, world!" << endl;
+
+    func(f1);
+    func(f3, 10);
+    func(f3, 20, 30);
+    func(f3, 20, 30, 40);
+    func(f3, 20, 30, 40, 50);
+    func(f3, 20, 30, 40, 50, 60);
+    func(f3, 20, 30, 40, 50, 60, 70);
 
     return 0;
 }
