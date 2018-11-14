c0f26979092ee4dc21ba719e30a079d0386cee2a
Jason Chen <shchenchang@gmail.com>
2018-11-14 17:27:33 +0800

    Add recursive variadic template parameter sample.

--- case13/main.cpp
+++ case13/main.cpp
@@ -21,7 +21,6 @@ void f3(int a, int b)
     cout << "f3_2p(" << a << ", " << b << ")" << endl;
 }
 
-
 void f3(int a, int b, int c)
 {
     cout << "f3_3p(" << a << ", " << b << ", " << c << ")" << endl;
@@ -31,6 +30,22 @@ template <typename ... Args> void f3(int a, int b, int c, Args ... args)
 {
     f3(a, b, c);
     f3(args ...);
+}
+
+void show()
+{
+    cout << "nothing" << endl;
+}
+
+template <typename T> void show(const T& t)
+{
+    cout << t << endl;
+}
+
+template <typename T, typename ... Args> void show(const T& t, Args ... args)
+{
+    show(t);
+    show(args ...);
 }
 
 int main()
@@ -44,6 +59,14 @@ int main()
     func(f3, 20, 30, 40, 50);
     func(f3, 20, 30, 40, 50, 60);
     func(f3, 20, 30, 40, 50, 60, 70);
+
+    show();
+    show(10);
+    show(20, 30);
+    show(20, 30, 40);
+    show(20, 30, 40, 50);
+    show(20, 30, 40, 50, 60);
+    show(20, 30, 40, 50, 60, 70);
 
     return 0;
 }
