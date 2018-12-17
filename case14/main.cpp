db33d3db406f898faa82eac1c76b0218ced149a9
Jason Chen <shchenchang@gmail.com>
2018-12-17 15:06:06 +0800

    Add `map::operator[]` sample.

--- case14/main.cpp
+++ case14/main.cpp
@@ -1,72 +1,40 @@
 #include <iostream>
+#include <map>
+#include <vector>
 using namespace std;
 
-template <typename ... Args> void func(void(*f)(Args ...), Args ... args)
+struct A
 {
-    (*f)(args ...);
-}
-
-void f1()
-{
-    cout << "f1()" << endl;
-}
-
-void f3(int n)
-{
-    cout << "f3_1p(" << n << ")" << endl;
-}
-
-void f3(int a, int b)
-{
-    cout << "f3_2p(" << a << ", " << b << ")" << endl;
-}
-
-void f3(int a, int b, int c)
-{
-    cout << "f3_3p(" << a << ", " << b << ", " << c << ")" << endl;
-}
-
-template <typename ... Args> void f3(int a, int b, int c, Args ... args)
-{
-    f3(a, b, c);
-    f3(args ...);
-}
-
-void show()
-{
-    cout << "nothing" << endl;
-}
-
-template <typename T> void show(const T& t)
-{
-    cout << t << endl;
-}
-
-template <typename T, typename ... Args> void show(const T& t, Args ... args)
-{
-    show(t);
-    show(args ...);
-}
+    A() { cout << "A()" << endl; }
+    int x, y;
+    vector<int> ivec;
+};
 
 int main()
 {
-    cout << "hello, world!" << endl;
+    map<int, int> imap;
 
-    func(f1);
-    func(f3, 10);
-    func(f3, 20, 30);
-    func(f3, 20, 30, 40);
-    func(f3, 20, 30, 40, 50);
-    func(f3, 20, 30, 40, 50, 60);
-    func(f3, 20, 30, 40, 50, 60, 70);
+    for (int i = 0; i < 5; i++)
+    {
+        imap[i] = i * 10;
+    }
 
-    show();
-    show(10);
-    show(20, 30);
-    show(20, 30, 40);
-    show(20, 30, 40, 50);
-    show(20, 30, 40, 50, 60);
-    show(20, 30, 40, 50, 60, 70);
+    for (const auto& item : imap)
+    {
+        cout << "imap[" << item.first << "]=" << item.second << endl;
+    }
+
+    for (int i = 0; i < 10; i++)
+    {
+        cout << "imap[" << i << "]=" << imap[i] << endl;
+    }
+
+    map<int, A> amap;
+    for (int i = 0; i < 10; i++)
+    {
+        cout << "imap[" << i << "]=" << amap[i].x << endl;
+    }
+    cout << amap.size() << endl;
 
     return 0;
 }
