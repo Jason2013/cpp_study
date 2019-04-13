74185087570dc878b074f7bd31e74231786d7535
Jason Chen <shchenchang@gmail.com>
2019-04-13 23:35:29 +0800

    Add the URL of the article: `Find memory leaks with the CRT library`

--- case08/main.cpp
+++ case08/main.cpp
@@ -1,3 +1,8 @@
+/*
+ * Find memory leaks with the CRT library
+ * https://docs.microsoft.com/en-us/visualstudio/debugger/finding-memory-leaks-using-the-crt-library?view=vs-2019
+ */
+
 #include "MemoryDebug.h" // muse be included first
 
 #include <iostream>
