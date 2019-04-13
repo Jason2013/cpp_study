8cf0a42c52b82f8bfdf13d3e1811ea3debe87157
Jason Chen <shchenchang@gmail.com>
2019-04-13 23:54:44 +0800

    Add the URL of the article: `Detecting Memory Leaks using the CrtDbg Library`

--- case08/main.cpp
+++ case08/main.cpp
@@ -1,6 +1,9 @@
 /*
  * Find memory leaks with the CRT library
  * https://docs.microsoft.com/en-us/visualstudio/debugger/finding-memory-leaks-using-the-crt-library?view=vs-2019
+ *
+ * Detecting Memory Leaks using the CrtDbg Library
+ * https://www.codeproject.com/articles/66324/detecting-memory-leaks-using-the-crtdbg-library
  */
 
 #include "MemoryDebug.h" // muse be included first
